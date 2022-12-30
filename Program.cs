0a95f7de2689f584f22c8d291b44be24b4cd13c5
m0hamedaymano <121689031+m0hamedaymano@users.noreply.github.com>
2022-12-30 20:22:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+
+Console.WriteLine("enter n1");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter n2");
+int n2 = int.Parse(Console.ReadLine());
+Console.WriteLine("");
+
+for (var i = n1; i <= n2; i++ )
+
+
+{
+    bool isprimenumber = true;
+
+    for (var j = 2; j < i; j++)
+    {
+        if(i % j ==0)
+        {
+            isprimenumber = false;
+            break;
+        }
+    }
+    if(isprimenumber)
+    {
+        Console.WriteLine(i + " - ");
+    }
+}
+
+
+
+
+
+
+
+
